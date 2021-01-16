a8a2e1a02eeb603cadb10370460ecb38ae89644b
FranciscoDadone <dadonefrancisco@hotmail.com.ar>
2021-01-16 15:52:44 -0300

    Added text editor. File sizes

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,21 @@
-var express = require('express');
-var router = express.Router();
-//var fileUpload = require('express-fileupload');
-var fs = require('fs-extra');
-var shell = require('shelljs');
+const express = require('express');
+const router = express.Router();
+const fs = require('fs-extra');
+const shell = require('shelljs');
 const loginViaCookie = require('../middlewares/loginViaCookie');
-var path = require('path');
+const path = require('path');
 const UserModel = require('../controller/models/User');
+const du = require('du');
 
 
 // Middlewares
-//router.use(fileUpload());
 router.use(express.static('public'));
 
-const busboy = require('connect-busboy');   // Middleware to handle the file upload https://github.com/mscdex/connect-busboy
+const busboy = require('connect-busboy');
 
 router.use(busboy({
-    highWaterMark: 2 * 1024 * 1024, // Set 2MiB buffer
-})); // Insert the busboy middle-ware
-
+    highWaterMark: 2 * 1024 * 1024, // 2MiB buffer
+})); 
 
 router.get('/:path?', loginViaCookie, (req,res) => {
     if(!req.isAuthenticated) {
@@ -28,7 +26,11 @@ router.get('/:path?', loginViaCookie, (req,res) => {
             username: req.username
         }, async (err, value) => {
             if(err) console.log(err);
-            let content = { files: [], directories: [] };
+            let content = { files: [], 
+                            directories: [],
+                            filesSizes: [],
+                            directoriesSizes: [] };
+
             let dir = path.join(__dirname + "/../public/storage/" + req.username);   
 
             if(req.params.path && req.query.dirname) { 
@@ -39,15 +41,58 @@ router.get('/:path?', loginViaCookie, (req,res) => {
                 dir = path.join(__dirname + "/../public/storage/" + req.username + "/" + req.query.dirname);
             }
             
+
+            const convertBytes = function(bytes) {
+                const sizes = ["Bytes", "KB", "MB", "GB", "TB"]
+              
+                if (bytes == 0) {
+                  return "n/a"
+                }
+              
+                const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+              
+                if (i == 0) {
+                  return bytes + " " + sizes[i]
+                }
+              
+                return (bytes / Math.pow(1024, i)).toFixed(1) + " " + sizes[i]
+            }
+
+            let totalSize = 0
+            const getAllFiles = function(dirPath, arrayOfFiles) {
+                files = fs.readdirSync(dirPath)
+              
+                arrayOfFiles = arrayOfFiles || [];
+              
+                files.forEach(function(file) {
+                  if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+                      return getAllFiles(path.join(dirPath, file))
+                  } else {
+                    arrayOfFiles.push(path.join(dirPath, file))
+                  }
+                })
+                arrayOfFiles.forEach(function(filePath) {
+                    totalSize += fs.statSync(filePath).size
+                })
+                return arrayOfFiles
+              }
+
             fs.readdir(dir, async (err, files) => {
+                if(err) { console.log(err); }
                 try {
                     files.forEach(file => {
                         if (fs.lstatSync(path.resolve(dir, file)).isDirectory()) {
                             content.directories.push(file);
+                            getAllFiles(path.resolve(dir, file));
+                    
+                            content.directoriesSizes.push(convertBytes(totalSize));
+                             
                         } else {
                             content.files.push(file);
+                            content.filesSizes.push(convertBytes(fs.lstatSync(path.resolve(dir, file)).size));
                         }
                     });
+
                     res.render('home', {
                         title: 'Cloud',
                         user: req.user,
@@ -97,41 +142,36 @@ router.post('/:path?', loginViaCookie, (req, res) => {
         }
     } else {
         if(req.busboy){
-                req.pipe(req.busboy); // Pipe it trough busboy
-                
-                req.busboy.on('file', (fieldname, file, filename) => {
-                    console.log(`Upload of '${filename}' started`);
+            req.pipe(req.busboy); // Pipe it trough busboy
+            
+            req.busboy.on('file', (fieldname, file, filename) => {
+                console.log(`Upload of '${filename}' started`);
+                var route = "./public/storage/" + req.username + "/" + filename, 
+                    dir = "./public/storage/" + req.username;
+                if(req.params.path == undefined && req.query.dirname) {
+                    route = "./public/storage/" + req.username + "/" + req.query.dirname + "/" + filename;
+                    dir = "./public/storage/" + req.username + "/" + req.query.dirname;
+                } else if(req.params.path != undefined && req.query.dirname == undefined) {
+                    route = "./public/storage/" + req.username + "/" + req.params.path.replace(/-/g, '/') + "/" + filename;
+                    dir = "./public/storage/" + req.username + "/" + req.params.path.replace(/-/g, '/');
+                } else if(req.params.path && req.query.dirname){
+                    route = "./public/storage/" + req.username + "/" + req.params.path.replace(/-/g, '/') + "/" + req.query.dirname + "/" + filename;
+                    dir = "./public/storage/" + req.username + "/" + req.params.path.replace(/-/g, '/') + "/" + req.query.dirname;
+                }
+                if(!fs.existsSync(dir)) {
+                    console.log("Creating directory: " + dir);
+                    shell.mkdir('-p', dir);
+                }
 
-                    var route = "./public/storage/" + req.username + "/" + filename, 
-                        dir = "./public/storage/" + req.username;
-
-                    if(req.params.path == undefined && req.query.dirname) {
-                        route = "./public/storage/" + req.username + "/" + req.query.dirname + "/" + filename;
-                        dir = "./public/storage/" + req.username + "/" + req.query.dirname;
-                    } else if(req.params.path != undefined && req.query.dirname == undefined) {
-                        route = "./public/storage/" + req.username + "/" + req.params.path.replace(/-/g, '/') + "/" + filename;
-                        dir = "./public/storage/" + req.username + "/" + req.params.path.replace(/-/g, '/');
-                    } else if(req.params.path && req.query.dirname){
-                        route = "./public/storage/" + req.username + "/" + req.params.path.replace(/-/g, '/') + "/" + req.query.dirname + "/" + filename;
-                        dir = "./public/storage/" + req.username + "/" + req.params.path.replace(/-/g, '/') + "/" + req.query.dirname;
-                    }
-
-                    if(!fs.existsSync(dir)) {
-                        console.log("Creating directory: " + dir);
-                        shell.mkdir('-p', dir);
-                    }
-
-                    const fstream = fs.createWriteStream(route);
-                    file.pipe(fstream);
-
-                    // On finish of the upload
-                    fstream.on('close', () => {
-                        console.log(`Upload of '${filename}' finished`);
-
-                        res.redirect('back'); //fallback
-                    });
+                const fstream = fs.createWriteStream(route);
+                file.pipe(fstream);
+                // On finish of the upload
+                fstream.on('close', () => {
+                    console.log(`Upload of '${filename}' finished`);
                 });
-            }
+            });
+            res.send('');
+        }
     }
 });
 
--- routes/media.js
+++ routes/media.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const loginViaCookie = require('../middlewares/loginViaCookie');
+const FileReader = require('filereader')
+const fs = require('fs');
 
 router.use(express.static('public'));
 
@@ -32,8 +34,6 @@ router.get('/image', loginViaCookie, (req, res) => {
 
     url += req.query.img;
 
-    console.log(url)
-
     res.render('images', {
         url: url,
         img: req.query.img,
@@ -43,32 +43,76 @@ router.get('/image', loginViaCookie, (req, res) => {
 
 router.get('/doc', loginViaCookie, (req, res) => {
 
-    let path = req.query.path.replace(/-/g, '/'), 
-        path1 = '', 
-        pstart = 0, 
-        pend = 0;
-    
-    if(path[0] == '/' || path[path.length - 1] == '/') {
-        if(path[0] == '/') { pstart = 1 }
-        if(path[path.length - 1] == '/') { pend = 1 }
 
-        for(let i = 0 + pstart; i < path.length - pend; i++) {
-            path1 += path[i];
+    let path = '';
+    if(req.query.path != undefined && req.query.path != '') {
+        path = req.query.path.replace(/([/])/g, '').replace(/-/g, '/');
+    } else {
+        path = '/';
+    }
+    console.log(path)
+    let url = 'public/storage/' + req.username,
+        webUrl = '/home/';
+    if(path != "") {
+        url += "/" + path + "/";
+        webUrl += path.replace(/([/])/g, '-');
+    } else {
+        url += "/";
+    }
+
+    if(req.query.dirname) {
+        url += req.query.dirname + "/";
+        if(path != '' && path != undefined) {
+            webUrl += "-" + req.query.dirname;
+        } else {
+            webUrl += req.query.dirname;
         }
     }
 
-    let url = 'storage/' + req.username;
-    if(path1 != "") {
-        url += "/" + path1 + "/" + req.query.file;
-    } else {
-        url += "/" + req.query.file;
-    }
+    url += req.query.file;
 
-    res.render('doc', {
-        url: url,
-        file: req.query.file
+    fs.readFile(url, 'utf8', function(err, data) {
+        if (err) {
+            res.render('doc', {
+                file: req.query.file,
+                url: url,
+                content: err,
+                webUrl: webUrl,
+                status: err
+            });
+        } else {
+            res.render('doc', {
+                file: req.query.file,
+                content: data,
+                url: url,
+                webUrl: webUrl,
+                status: ''
+            });
+        }
     });
+});
 
+router.post('/saveDoc', loginViaCookie, (req, res) => {
+
+    fs.writeFile(req.body.url, req.body.textarea, function(err) {
+        if(err){ 
+            res.render('doc', {
+                file: req.body.file,
+                url: req.body.url,
+                content: req.body.textarea,
+                webUrl: req.body.webUrl,
+                status: 'err'
+            });
+        } else {
+            res.render('doc', {
+                file: req.body.file,
+                url: req.body.url,
+                content: req.body.textarea,
+                webUrl: req.body.webUrl,
+                status: 'OK'
+            });
+        }
+    });    
 });
 
 
