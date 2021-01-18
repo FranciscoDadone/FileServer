848f704ff64294044e7300fa97ecbca315a1f159
Francisco Dadone <dadonefrancisco@hotmail.com.ar>
2021-01-17 23:29:42 -0300

    deleting mongo uri

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,7 @@ const {
     MONGO_URI
 } = process.env;
 
-const MONGO_URI2 = 'mongodb+srv://fileserver:xXlQmYB5zo0OiokJ@cluster0.hazwq.mongodb.net/fileserver?retryWrites=true&w=majority';
+const MONGO_URI2 = '';
 
 module.exports = {
     mongoURI: MONGO_URI || MONGO_URI2
